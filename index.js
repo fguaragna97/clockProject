9e4c88cfa6f1ddbf336b356aaf8c9f69ff114211
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2021-10-11 22:57:21 -0700

    UpdateFunctions and readme

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 //we need to use transform rotate for the clock hands , and give transition timing function
 
+function getDate() {
+  const time = new Date().getTime();
+  const date = new Date(time);
+  const day = date.toString();
+
+  // getting the hour , minute and seconds
+  let minute = date.getMinutes();
+  const minuteDegrees = (minute / 60) * 360 + 90;
+  let hour = date.getHours();
+  const hourDegrees = (hour / 12) * 360 + 90;
+  let second = date.getSeconds();
+  const secondsDegrees = (second / 60) * 360 + 90;
+
+  // get elements
+  let hours = document.getElementById("hours");
+  let minutes = document.getElementById("minutes");
+  let seconds = document.getElementById("seconds");
+
+  //change the style
+  hours.style.transform = "rotate(" + hourDegrees + "deg)";
+  minutes.style.transform = "rotate(" + minuteDegrees + "deg)";
+  seconds.style.transform = "rotate(" + secondsDegrees + "deg)";
+}
+
 window.addEventListener("load", async () => {
   try {
-    setInterval(function () {
-      const time = new Date().getTime();
-      const date = new Date(time);
-      const day = date.toString();
-
-      // getting the hour , minute and seconds
-      let minute = date.getMinutes();
-      const minuteDegrees = (minute / 60) * 360 + 90;
-      let hour = date.getHours();
-      const hourDegrees = (hour / 12) * 360 + 90;
-      let second = date.getSeconds();
-      const secondsDegrees = (second / 60) * 360 + 90;
-
-      console.log(hour, minute, second);
-      // get elements
-      let hours = document.getElementById("hours");
-      let minutes = document.getElementById("minutes");
-      let seconds = document.getElementById("seconds");
-
-      //change the style
-      hours.style.transform = "rotate(" + hourDegrees + "deg)";
-      minutes.style.transform = "rotate(" + minuteDegrees + "deg)";
-      seconds.style.transform = "rotate(" + secondsDegrees + "deg)";
-    }, 1000);
+    setInterval(getDate, 1000);
   } catch (error) {
     console.error(error);
   }
